350aeecb5f6766cfb6134fd9bb28e8c1d5d72ef7
Lindsay jo Walker <lindsayjowalker@lindsays-mbp.lan>
2023-02-27 08:51:20 -0600

    working with day7

--- day7/tests/Ballot.ts
+++ day7/tests/Ballot.ts
@@ -1,5 +1,6 @@
+import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Ballot } from "../typechain-types/Ballot";
+import { Ballot } from "../typechain-types";
 
 const PROPOSALS = ["Alice", "Bob", "Chuck"];
 
@@ -19,12 +20,34 @@ describe ("Ballot", function () {
             convertStringArrayToBytes32(PROPOSALS)
             );
         console.log("heyooo");
-        // await ballotContract.deployTransaction.wait();∏
+        await ballotContract.deployTransaction.wait();
     });
+    
     describe ("When the contract is deployed", function () {
-    });
-    describe ("Has the provided proposals", function () {
-    });
-    describe ("sets depolyer address as chairperson", function () {
-    });
+        it ("Has the provided proposals", async function () {
+            for (let index = 0; index < PROPOSALS.length; index++){
+                const proposal = await ballotContract.proposals(index);
+                expect(ethers.utils.parseBytes32String(proposal.name)).to.eq(
+                    PROPOSALS[index]);  
+            }
+        });
+        it ("sets depolyer address as chairperson", async function () {
+            const signers = await ethers.getSigners();
+            const deployer = signers[0].address;
+            const chairperson = await ballotContract.chairperson();
+            expect(chairperson).to.eq(deployer);
+        });
+        it ("has zero votes for all proposals", async function () {
+            for (let index = 0; index < PROPOSALS.length; index++){
+                const proposal = await ballotContract.proposals(index);
+                expect(proposal.voteCount).to.eq(0);  
+            }
+        });
+        it ("sets voting weight for chairperson as 1", async function () {
+            const chairperson = await ballotContract.chairperson();
+            const chairpersonVoter = await ballotContract.voters(chairperson);
+            const votingWeight = chairpersonVoter.weight;
+            expect(votingWeight).to.eq(1);
+        });
+     });
 });
